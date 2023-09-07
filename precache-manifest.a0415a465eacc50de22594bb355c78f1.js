898cc73d01097f48a1502087037ece71c3466bae
Утбанов Александр <skilldilldark@yandex.ru>
2023-09-07 21:43:53 +0300

    Updates

---
+++ precache-manifest.a0415a465eacc50de22594bb355c78f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb234aa02987255c909e8db59d596c5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "acdc8105f42cdda5705d",
+    "url": "./static/css/main.a851ea40.chunk.css"
+  },
+  {
+    "revision": "f8ddec0fe80d704dfddd",
+    "url": "./static/js/2.808f4fcd.chunk.js"
+  },
+  {
+    "revision": "e97f0ae9fcd1cfdff2ffcc983791cfa2",
+    "url": "./static/js/2.808f4fcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acdc8105f42cdda5705d",
+    "url": "./static/js/main.ce75c4d4.chunk.js"
+  },
+  {
+    "revision": "f8f781c85790b9db5cc0",
+    "url": "./static/js/runtime-main.9c23173a.js"
+  }
+]);
